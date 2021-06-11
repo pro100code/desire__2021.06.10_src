0587341c29d0062c69c5f1594cab9af5e5bd774d
Oleh Yefimenko <oleh.yefimenko@gmail.com>
2021-06-11 23:35:41 +0300

    закончил footer

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -3,16 +3,16 @@ if (document.querySelector('.menu__link_btn')) {
 	const menuLinkBtn = document.querySelector('.menu__link_btn');
 	const rightside = document.querySelector('.rightside');
 	const rightsideClose = document.querySelector('.rightside__close');
+	let rightsideCloseActive = rightsideClose.classList.add('_active');
 
 	menuLinkBtn.addEventListener("click", function () {
 		rightsideClose.classList.remove("_active");
-		rightside.classList.toggle("_active");
-		rightsideClose.classList.toggle("_active");
+		rightside.classList.add("_active");
+		rightsideClose.classList.add("_active");
 	});
 
 	rightsideClose.addEventListener("click", function () {
 		rightside.classList.remove("_active");
 		rightsideClose.classList.remove("_active");
-
 	});
 }
